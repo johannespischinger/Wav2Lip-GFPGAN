d2aa8eabe7f5678800e16d90a63e3ada0583c5c7
johannespischinger <johannes.pischinger@tum.de>
2023-07-05 17:20:48 +0200

    Adding main:app

---
+++ Wav2Lip-master/main.py
@@ -0,0 +1,85 @@
+import os
+import shutil
+
+from fastapi import FastAPI, File, UploadFile, Form
+from pydantic import BaseModel
+from inference import Inference
+from fastapi.responses import FileResponse
+import boto3
+
+app = FastAPI()
+
+
+class LipSync(BaseModel):
+    videoURL: str
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello sweetheart, lets upload some files to the API"}
+
+@app.get("/files/{file_path:path}")
+async def get_file(file_path: str):
+    return FileResponse(file_path)
+
+
+
+@app.post("/inference")
+async def inference(audio: UploadFile = File(...), video: UploadFile = File(...), video_name: str= Form(...)):
+    # Save the video file
+    video_filename = f"inputs/{video.filename}"
+    with open(video_filename, "wb") as f:
+        f.write(await video.read())
+
+    # Save the audio file
+    audio_filename = f"inputs/{audio.filename}"
+    with open(audio_filename, "wb") as f:
+        f.write(await audio.read())
+
+    # Add your processing code here
+
+    # Set the default output file name if video_name is not provided
+    if video_name:
+        output_file_name = video_name
+    else:
+        output_file_name = "output"
+
+    # Set the output file path
+    output_file_path = f"results/{output_file_name}"
+    inference = Inference(video=video_filename, audio=audio_filename, outputFile=output_file_path)
+    output_file = inference.run()
+
+    # Rename the file if a file name is provided
+    # if video_name:
+    #     new_file_path = f"uploads/{video_name}"
+    #     shutil.move(output_file, new_file_path)
+    #     output_file = new_file_path
+
+    s3_bucket_name = "dev.susio.videogeneration"
+    s3_key = f"cache/{video_name}"
+    s3_client = boto3.client("s3")
+    s3_client.upload_file(output_file, s3_bucket_name, s3_key)
+
+    # Generate the S3 URL for the uploaded video
+    video_url = f"https://{s3_bucket_name}.s3.amazonaws.com/{s3_key}"
+
+    # Delete the local files
+    os.remove(video_filename)
+    os.remove(audio_filename)
+
+    return {
+        "message": "File uploaded successfully",
+        "video_url": video_url
+    }
+        # LipSync(video_url=video_url)
+
+    # with open(fileLocation, "wb+") as file_object:
+    #     file_object.write(fileLocation)
+    # return {'message': FileResponse(fileLocation, media_type="video/mp4")}
+    # return {"info": f"file '{outputFileName}' saved at '{fileLocation}'"}
+
+    # # Return the processed filenames
+    # filename = os.path.basename(urlOutputFile)
+    # headers = {'Content-Disposition': f'attachment; filename="{filename}"'}
+    #
+    # return {'message': FileResponse(urlOutputFile, headers=headers, media_type="video/mp4")}
